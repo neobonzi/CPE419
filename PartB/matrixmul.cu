2813cbb1bd04f133979dc076cd640c8ea1946827
Jon Doughty <jdoughty@calpoly.edu>
2015-09-28 17:18:52 -0700

    Added several functions:
    1. function to allocate memory
    2. function to store mapped data into 1D array
    3. function to setup GPU allocations and send data to GPU.
    4. sketched out main.

--- PartB/matrixmul.cu
+++ PartB/matrixmul.cu
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
+#define DEF_ROWS 2
+#define DEF_COLS 2
+
 // FLOAT will either be a float or double depending on what user decides. (could use a better name)
 typedef struct {
   int rows;
@@ -96,7 +99,25 @@ void writeOutput(Matrix *mat){
 }
 
 /**
-* If array holding matrix data is not big enough create a new one twice as big.
+* Initialize an array in memory to hold matrix data.
+*/
+void initMatrixArray(Matrix *mat, int initRows, int initCols) {
+  mat->rows = initRows;
+  mat->cols = initCols;
+    
+  FLOAT *newArray = (FLOAT *) malloc(sizeof(FLOAT) * mat->rows * mat->cols);
+  
+  if (newArray == NULL) {
+    perror("Error, couldn't allocate space for array");
+    exit(1);
+  }
+  
+  // set pointer to new array in memory
+  mat->arr = newArray;
+}
+
+/**
+* If array holding Matrix data is not big enough create a new one twice as big.
 * Copy old array data to new array, and free old array from memory.
 */
 void doubleArraySize(Matrix *mat) {
@@ -118,17 +139,112 @@ void doubleArraySize(Matrix *mat) {
   mat->arr = newArray;
 }
 
+/**
+* Read values from memory mapped location into an array for processing.
+* This function reads in the characters and converts them to floats or doubles
+* before storing in the array.
+*/
+void storeMatrixToArray(Matrix *mat){
+  int mmapIdx = 0, bfrIdx = 0, arrIdx = 0, numRows = 0, numCols = 0, countCols = 1;
+  int matSize = mat->rows * mat->cols;
+  char buffer[101];    // buffer to hold FLOAT up to 100 digits long
+  
+  for(mmapIdx = 0; mmapIdx <= mat->mmapFileSize; mmapIdx++) {
+    if(mat->mmapFileLoc[mmapIdx] == ' '){ // found a number, store into Matrix
+      // null-terminate the buffer so it can be passed to atof()
+      buffer[bfrIdx] = '\0'; 
+      
+      // convert char buffer to FLOAT and store in Matrix array
+      mat->arr[arrIdx++] = (FLOAT) atof(buffer);  
+      
+      // if array is full, double size
+      if (arrIdx == matSize) { 
+        doubleArraySize(mat); 
+      }
+      
+      // only count number of columns until reach first newline
+      if (countCols) numCols++;  
+      
+      // clear buffer, reset buffer index to 0
+      memset(buffer, '\0', bfrIdx); 
+      bfrIdx = 0;
+    } else if(mat->mmapFileLoc[mmapIdx] == '\n') {              
+      if (countCols) {
+        mat->cols = numCols;  
+        countCols = 0;    // disable counting columns
+      }   
+      numRows++;
+    }
+    
+    /* grab a character at each loop iteration and store into buffer[] to conv to FLOAT */
+    buffer[bfrIdx++] = mat->mmapFileLoc[mmapIdx];    
+  }
+  
+  mat->rows = numRows;
+}
+
+void matrixMulOnDevice(Matrix *mat1, Matrix *mat2, Matrix *mat3){
+  int size = mat1->cols * mat1->cols * sizeof(FLOAT);
+  FLOAT *Md, *Nd, *Pd;
+  
+  // Allocate space and send data for Matrix1 to GPU
+  HANDLE_ERROR( cudaMalloc(&Md, size) );
+  HANDLE_ERROR( cudaMemcpy(Md, mat1->arr, size, cudaMemcpyHostToDevice) );
+  
+  // Allocate space and send data for Matrix2 to GPU
+  HANDLE_ERROR( cudaMalloc(&Nd, size) );
+  HANDLE_ERROR( cudaMemcpy(Nd, mat2->arr, size, cudaMemcpyHostToDevice) );
+  
+  // Allocate space for Matrix3 on GPU
+  HANDLE_ERROR( cudaMalloc(&Pd, size) );
+  
+  // Call GPU Matrix multiply function here.
+    // Not yet implemented
+    
+  // Copy results back to CPU
+  HANDLE_ERROR( cudaMemcpy(mat3->arr, Pd, size, cudaMemcpyDeviceToHost) );
+  HANDLE_ERROR( cudaFree(Pd) );
+}
+
 int main( int argc, char **argv ) {
-  int c;
-  int *dev_c;
-  HANDLE_ERROR( cudaMalloc( (void**)&dev_c, sizeof(int) ) );
+  // exit if not enough arguments
+  if (argc != 3) {
+    printf("Error: incorrect amount of arguments");
+    exit(1);
+  }
 
-  //mult<<<1,1>>>( 2, 7, dev_c );
+  // Setup matrix1
+  Matrix m1;
+  Matrix *pMat1 = &m1;
+  mapFileToMemory(argv[1], pMat1);
+  initMatrixArray(pMat1, DEF_ROWS, DEF_COLS);
+  storeMatrixToArray(pMat1);
+  unmapFile(pMat1);
 
-  HANDLE_ERROR( cudaMemcpy( &c, dev_c, sizeof(int),
-        cudaMemcpyDeviceToHost ) );
-  printf( "2 * 7 = %d\n", c );
-  HANDLE_ERROR( cudaFree( dev_c ) );
+  // Setup matrix2
+  Matrix m2;
+  Matrix *pMat2 = &m2;
+  mapFileToMemory(argv[2], pMat2);
+  initMatrixArray(pMat2, DEF_ROWS, DEF_COLS);
+  storeMatrixToArray(pMat2);
+  unmapFile(pMat2);
+
+  // check that matrix is square
+  if(pMat1->rows != pMat2->rows){
+    printf("Error: matrix is not square\n");
+    exit(1);
+  }
+ 
+  // Setup matrix3
+  Matrix m3;
+  Matrix *pMat3 = &m3;
+  initMatrixArray(pMat3, pMat1->rows, pMat1->cols);
+  matrixMulOnDevice(pMat1, pMat2, pMat3);
+  writeOutput(pMat3);
+  
+  free(pMat1->arr);
+  free(pMat2->arr);
+  free(pMat3->arr);
 
   return 0;
 }
