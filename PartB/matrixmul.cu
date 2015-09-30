e8ade64cdaeb75e1ea011dac9c1dc1ac132c79fd
Jon Doughty <jdoughty@calpoly.edu>
2015-09-30 14:34:30 -0700

    Updated storeMatrixToArray

--- PartB/matrixmul.cu
+++ PartB/matrixmul.cu
@@ -8,7 +8,7 @@
 
 #define DEF_ROWS 2
 #define DEF_COLS 2
-#define TILE_WIDTH 
+#define TILE_WIDTH
 
 // FLOAT will either be a float or double depending on what user decides. (could use a better name)
 typedef struct {
@@ -17,6 +17,7 @@ typedef struct {
   FLOAT *arr;          // array of data
   int mmapFileSize;    // size of file mapped to memory
   char *mmapFileLoc;   // pointer to file mapped to memory
+  int size;
 } Matrix;
 
 static void HandleError( cudaError_t err,
@@ -33,14 +34,14 @@ static void HandleError( cudaError_t err,
 
 /**
  * This function will multiply a single row and column in a matrix
- */ 
+ */
 __global__ void MatMulKernel(FLOAT *Md, FLOAT *Nd, FLOAT *Pd, int width, int height) {
     //Get row and col in matrix that we are responsible for
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
 
     float accum = 0;
-    //Iterate through 
+    //Iterate through
     for(int k=0; k < width; k++)
     {
         accum += Md[row * width + k] * Nd[k * width + col];
@@ -55,27 +56,27 @@ int mapFileToMemory(char* fName, Matrix *mat){
   int fd, size;
   char *map;
   struct stat st;
-  
+
   stat(fName, &st);
   size = st.st_size;      // use stat to get file size
   mat->mmapFileSize = size;
-  
+
   fd = open(fName, O_RDONLY);
   if (fd == -1) {
     perror("Error, could not open file");
     close(fd);
     exit(1);
   }
-  
+
   map = (char*) mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
   if (map == MAP_FAILED) {
     perror("Error, could not map file");
     close(fd);
     exit(1);
   }
-  
+
   mat->mmapFileLoc = map;
-  
+
   close(fd);
   return  0;
 }
@@ -88,7 +89,7 @@ int unmapFile(Matrix *mat){
   	perror("Error un-mapping the file");
   	exit(1);
   }
-  
+
   return 0;
 }
 
@@ -102,7 +103,7 @@ void writeOutput(Matrix *mat){
     perror("Could not open result.out to write results");
     exit(1);
   }
-  
+
   int i,j;
   for(i = 0; i < mat->rows; i++){
     for(j = 0; j < mat->cols; j++){
@@ -111,7 +112,7 @@ void writeOutput(Matrix *mat){
     // print newline for all rows
     fprintf(ofp, "\n");
   }
-  
+
   // close output file pointer
   fclose(ofp);
 }
@@ -122,14 +123,15 @@ void writeOutput(Matrix *mat){
 void initMatrixArray(Matrix *mat, int initRows, int initCols) {
   mat->rows = initRows;
   mat->cols = initCols;
-    
+  mat->size = mat->rows * mat->cols;
+
   FLOAT *newArray = (FLOAT *) malloc(sizeof(FLOAT) * mat->rows * mat->cols);
-  
+
   if (newArray == NULL) {
     perror("Error, couldn't allocate space for array");
     exit(1);
   }
-  
+
   // set pointer to new array in memory
   mat->arr = newArray;
 }
@@ -140,19 +142,22 @@ void initMatrixArray(Matrix *mat, int initRows, int initCols) {
 */
 void doubleArraySize(Matrix *mat) {
   // malloc new array, double the size of previous array
-  FLOAT *newArray = (FLOAT *) malloc(sizeof(FLOAT) * mat->rows * mat->cols * 2);
-  
+  FLOAT *newArray = (FLOAT *) malloc(sizeof(FLOAT) * mat->size * 2);
+
   if (newArray == NULL) {
     perror("Error, couldn't allocate space for array\n");
     exit(1);
   }
-  
+
   // copy old array to newArray
-  newArray = (FLOAT*) memcpy(newArray, mat->arr, sizeof(FLOAT) * mat->rows * mat->cols);
-  
+  newArray = (FLOAT*) memcpy(newArray, mat->arr, sizeof(FLOAT) * mat->size);
+
+  // update size of array
+  mat->size *= 2;
+
   // free the old array
   free(mat->arr);
-  
+
   // set pointer to new array in memory
   mat->arr = newArray;
 }
@@ -164,40 +169,39 @@ void doubleArraySize(Matrix *mat) {
 */
 void storeMatrixToArray(Matrix *mat){
   int mmapIdx = 0, bfrIdx = 0, arrIdx = 0, numRows = 0, numCols = 0, countCols = 1;
-  int matSize = mat->rows * mat->cols;
   char buffer[101];    // buffer to hold FLOAT up to 100 digits long
-  
+
   for(mmapIdx = 0; mmapIdx <= mat->mmapFileSize; mmapIdx++) {
     if(mat->mmapFileLoc[mmapIdx] == ' '){ // found a number, store into Matrix
       // null-terminate the buffer so it can be passed to atof()
-      buffer[bfrIdx] = '\0'; 
-      
-      // convert char buffer to FLOAT and store in Matrix array
-      mat->arr[arrIdx++] = (FLOAT) atof(buffer);  
-      
+      buffer[bfrIdx] = '\0';
+
       // if array is full, double size
-      if (arrIdx == matSize) { 
-        doubleArraySize(mat); 
+      if (arrIdx == mat->size) {
+        doubleArraySize(mat);
       }
-      
+
+      // convert char buffer to FLOAT and store in Matrix array
+      mat->arr[arrIdx++] = (FLOAT) atof(buffer);
+
       // only count number of columns until reach first newline
-      if (countCols) numCols++;  
-      
+      if (countCols) numCols++;
+
       // clear buffer, reset buffer index to 0
-      memset(buffer, '\0', bfrIdx); 
+      memset(buffer, '\0', bfrIdx);
       bfrIdx = 0;
-    } else if(mat->mmapFileLoc[mmapIdx] == '\n') {              
+    } else if(mat->mmapFileLoc[mmapIdx] == '\n') {
       if (countCols) {
-        mat->cols = numCols;  
+        mat->cols = numCols;
         countCols = 0;    // disable counting columns
-      }   
+      }
       numRows++;
     }
-    
+
     /* grab a character at each loop iteration and store into buffer[] to conv to FLOAT */
-    buffer[bfrIdx++] = mat->mmapFileLoc[mmapIdx];    
+    buffer[bfrIdx++] = mat->mmapFileLoc[mmapIdx];
   }
-  
+
   mat->rows = numRows;
 }
 
@@ -207,43 +211,43 @@ int errorCheckMatrices(Matrix *mat1, Matrix *mat2){
     printf("Error: matrix1 is not square %dx%d\n", mat1->rows, mat1->cols);
     exit(1);
   }
-  
+
   // check that matrix2 is square
   if(mat2->rows != mat2->cols){
     printf("Error: matrix2 is not square %dx%d\n",mat2->rows, mat2->cols);
     exit(1);
   }
-  
+
   // check that matrix1 is same size as matrix2
   if(mat1->cols != mat2->rows){
     printf("Error: matrices are not compatible size\n");
     exit(1);
   }
-  
+
   return 0;
 }
 void matrixMulOnDevice(Matrix *mat1, Matrix *mat2, Matrix *mat3){
   int size = mat1->cols * mat1->cols * sizeof(FLOAT);
   FLOAT *Md, *Nd, *Pd;
-  
+
   // Allocate space and send data for Matrix1 to GPU
   HANDLE_ERROR( cudaMalloc(&Md, size) );
   HANDLE_ERROR( cudaMemcpy(Md, mat1->arr, size, cudaMemcpyHostToDevice) );
-  
+
   // Allocate space and send data for Matrix2 to GPU
   HANDLE_ERROR( cudaMalloc(&Nd, size) );
   HANDLE_ERROR( cudaMemcpy(Nd, mat2->arr, size, cudaMemcpyHostToDevice) );
-  
+
   // Allocate space for Matrix3 on GPU
   HANDLE_ERROR( cudaMalloc(&Pd, size) );
-  
+
   // Each block only holds 32 rows & cols so numBlocks in any direciton
   // i.e. numBlocksX = matrixWidth / 32
   dim3 dimGrid(mat1->cols/32, mat1->cols/32);
   // 1024 threads per blocks, square in our case so all must be 32x32
   dim3 dimBlock(32, 32);
   MatMulKernel<<<dimGrid, dimBlock>>>(Md, Nd, Pd, mat1->cols, mat2->cols);
-    
+
   // Copy results back to CPU
   HANDLE_ERROR( cudaMemcpy(mat3->arr, Pd, size, cudaMemcpyDeviceToHost) );
   HANDLE_ERROR( cudaFree(Pd) );
@@ -273,14 +277,14 @@ int main( int argc, char **argv ) {
   unmapFile(pMat2);
 
   errorCheckMatrices(pMat1, pMat2);
- 
+
   // Matrix3 setup and compute
   Matrix m3;
   Matrix *pMat3 = &m3;
   initMatrixArray(pMat3, pMat1->rows, pMat1->cols);
   matrixMulOnDevice(pMat1, pMat2, pMat3);
   writeOutput(pMat3);
-  
+
   // Free allocated memory
   free(pMat1->arr);
   free(pMat2->arr);
@@ -288,4 +292,3 @@ int main( int argc, char **argv ) {
 
   return 0;
 }
-
