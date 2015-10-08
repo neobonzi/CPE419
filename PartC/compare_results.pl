e8b9719a660a12c2f1e3673e20eb4c44509e180b
James Bilous <JBillz@pcp143017pcs.wireless.calpoly.edu>
2015-10-08 14:06:57 -0700

    Updated report for Part C

--- PartC/compare_results.pl
+++ PartC/compare_results.pl
@@ -4,30 +4,30 @@ use strict;
 use Test::Number::Delta within => 1.0, tests => 1;
 
 my $goldfile = shift;
-my <file = shift;
+my $tfile = shift;
 my @gold;
 my @result;
 
 # open file
 open(GOLD, "$goldfile" ) or die("Unable to open $goldfile");
-open(TEST, "<file" ) or die("Unable to open <file");
+open(TEST, "$tfile" ) or die("Unable to open $tfile");
 
 # read file into an array
 while (<GOLD>) {
   chomp;
-    @gold = split(/ /,$_); 
-    }
+  @gold = split(/ /,$_); 
+}
 
-    while (<TEST>) {
-      chomp;
-        @result = split(/ /,$_);
-        }
+while (<TEST>) {
+  chomp;
+  @result = split(/ /,$_);
+}
 
-        foreach my $g (@gold) {
-        }
+foreach my $g (@gold) {
+}
 
-        delta_ok( \@gold, \@result, 'Values within tolerance.');
+delta_ok( \@gold, \@result, 'Values within tolerance.');
 
-        # close file 
-        close(GOLD);
-        close(TEST);
+# close file 
+close(GOLD);
+close(TEST);
