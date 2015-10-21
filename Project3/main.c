94794b693f55c5a7a320bc274a08978addc4a67d
Jon Doughty <jdoughty@calpoly.edu>
2015-10-21 10:35:15 -0700

    framework

--- Project3/main.c
+++ Project3/main.c
@@ -9,28 +9,22 @@
 #define DEF_ROWS 2
 #define DEF_COLS 2
 
-// FLOAT will either be a float or double depending on what user decides.
-typedef struct {
-  int rows;
-  int cols;
-  FLOAT *arr;          // array of data
-  int mmapFileSize;    // size of file mapped to memory
-  char *mmapFileLoc;   // pointer to file mapped to memory
-  int size;
-} Matrix;
+typedef struct{
+  float *arr;
+} Vector;
 
 /**
 * This function will take a filename and map its contents into memory for 
 * faster access.
 */
-int mapFileToMemory(char* fName, Matrix *mat){
+int mapFileToMemory(char* fName, Vector *vec){
   int fd, size;
   char *map;
   struct stat st;
 
   stat(fName, &st);
   size = st.st_size;      // use stat to get file size
-  mat->mmapFileSize = size;
+  vec->mmapFileSize = size;
 
   fd = open(fName, O_RDONLY);
   if (fd == -1) {
@@ -46,7 +40,7 @@ int mapFileToMemory(char* fName, Matrix *mat){
     exit(1);
   }
 
-  mat->mmapFileLoc = map;
+  vec->mmapFileLoc = map;
 
   close(fd);
   return  0;
@@ -55,8 +49,8 @@ int mapFileToMemory(char* fName, Matrix *mat){
 /**
 * This function will un-map a file that was previously mapped into memory.
 */
-int unmapFile(Matrix *mat){
-  if (munmap(mat->mmapFileLoc, mat->mmapFileSize) == -1) {
+int unmapFile(Vector *vec){
+  if (munmap(vec->mmapFileLoc, vec->mmapFileSize) == -1) {
   	perror("Error un-mapping the file");
   	exit(1);
   }
@@ -65,137 +59,14 @@ int unmapFile(Matrix *mat){
 }
 
 /**
-* Takes 3 matrices, multiples p1 x p2 and stores result in p3.
+* Initialize an array in memory to hold vecrix data.
 */
-void computeMatrix(Matrix *p1, Matrix *p2, Matrix *p3){
-  // check rows and colums to see if both matrices are square
-  int i, j, k;
-  
-  //variables declared outside pragma are shared by default, so set to private
-  #pragma omp parallel private(i,j,k)
-  {
-    #pragma omp for schedule(dynamic)
-    for(i = 0; i < p1->rows; i++) {
-      for(j = 0; j < p2->cols; j++){
-        // Compute C[i,j] as the vector product of A[i,*]B[*,j]
-	      p3->arr[i*(p3->cols)+j] = 0.00; // test make sure all values start at zero
-	      for(k = 0; k < p1->cols; k++) {
- 	        p3->arr[i*(p3->cols)+j] +=  p1->arr[i*(p1->cols)+k] * p2->arr[k*(p2->cols)+j];
-        }
-      }
-    }
-  }
-}
+void initVectorArray(Vector *vec, int initRows, int initCols) {
+  vec->rows = initRows;
+  vec->cols = initCols;
+  vec->size = vec->rows * vec->cols;
 
-/**
-* Takes 3 matrices, multiples p1 x p2 and stores result in p3.
-* This function uses memory stored in a different fashion for faster access.
-* Created a separate function so that program doesn't need to check additional 
-* flag each time in loop.
-*/
-void computeMatrixPrime(Matrix *p1, Matrix *p2, Matrix *p3){
-  // check rows and colums to see if both matrices are square
-  int i, j, k;
-
-  //variables declared outside pragma are shared by default, so set to private
-  #pragma omp parallel private(i,j,k)
-  {
-    #pragma omp for schedule(dynamic)
-    for(i = 0; i < p1->rows; i++) {
-      for(j = 0; j < p2->cols; j++){
-        // Compute C[i,j] as the vector product of A[i,*]B[*,j]
-        p3->arr[i*(p3->cols)+j] = 0.00; // test make sure all values start at zero
-        for(k = 0; k < p1->cols; k++) {
-          p3->arr[i*(p3->cols)+j] += p1->arr[i*(p1->cols)+k] * p2->arr[j*(p2->rows)+k];
-        }
-      }
-    }
-  }
-}
-
-/**
-* Write results to a file named "results.out"
-* Data in mat is stored row-major order.
-*/
-void writeOutput(Matrix *mat){
-  FILE* ofp;
-  ofp = fopen("result.out","w");
-  if(ofp == NULL) {
-    perror("Could not open result.out to write results");
-    exit(1);
-  }
-
-  int i,j;
-  for(i = 0; i < mat->rows; i++){
-    for(j = 0; j < mat->cols; j++){
-      fprintf(ofp, "%.2f ", mat->arr[i * mat->cols + j]);
-    }
-    // print newline for all rows
-    fprintf(ofp, "\n");
-  }
-
-  // close output file pointer
-  fclose(ofp);
-}
-
-/**
-* Write results to a file named "results.out"
-* Data in mat is stored column-major order.
-*/
-void writeOutputColMajor(Matrix *mat){
-  FILE* ofp;
-  ofp = fopen("result.out","w");
-  if(ofp == NULL) {
-    perror("Could not open result.out to write results");
-    exit(1);
-  }
-
-  int i,j;
-  for(i = 0; i < mat->rows; i++){
-    for(j = 0; j < mat->cols; j++){
-      fprintf(ofp, "%.2f ", mat->arr[i + mat->rows * j]);
-    }
-    // print newline for all rows
-    fprintf(ofp, "\n");
-  }
-
-  // close output file pointer
-  fclose(ofp);
-}
-
-/**
-* Print the contents of matrix to stdout for debugging
-*/
-void printMatrix(Matrix *mat) {
-  int i,j;
-  for(i = 0; i < mat->rows; i++){
-    for(j = 0; j < mat->cols; j++){
-      printf("%.2f ", mat->arr[i * mat->cols + j]);
-    }
-    // print newline for all rows
-    printf("\n");
-  }
-}
-
-/**
-* Print the contents of matrix to stdout for debugging
-*/
-void printMatrixAll(Matrix *mat) {
-  int i;
-  for(i = 0; i < mat->rows * mat->cols; i++){
-    printf("%.2f ", mat->arr[i]);
-  }
-}
-
-/**
-* Initialize an array in memory to hold matrix data.
-*/
-void initMatrixArray(Matrix *mat, int initRows, int initCols) {
-  mat->rows = initRows;
-  mat->cols = initCols;
-  mat->size = mat->rows * mat->cols;
-
-  FLOAT *newArray = (FLOAT *) malloc(sizeof(FLOAT) * mat->rows * mat->cols);
+  FLOAT *newArray = (FLOAT *) malloc(sizeof(FLOAT) * vec->rows * vec->cols);
 
   if (newArray == NULL) {
     perror("Error, couldn't allocate space for array");
@@ -203,16 +74,16 @@ void initMatrixArray(Matrix *mat, int initRows, int initCols) {
   }
 
   // set pointer to new array in memory
-  mat->arr = newArray;
+  vec->arr = newArray;
 }
 
 /**
-* If array holding Matrix data is not big enough create a new one twice as big.
+* If array holding Vector data is not big enough create a new one twice as big.
 * Copy old array data to new array, and free old array from memory.
 */
-void doubleArraySize(Matrix *mat) {
+void doubleArraySize(Vector *vec) {
   // malloc new array, double the size of previous array
-  FLOAT *newArray = (FLOAT *) malloc(sizeof(FLOAT) * mat->size * 2);
+  FLOAT *newArray = (FLOAT *) malloc(sizeof(FLOAT) * vec->size * 2);
 
   if (newArray == NULL) {
     perror("Error, couldn't allocate space for array\n");
@@ -220,16 +91,16 @@ void doubleArraySize(Matrix *mat) {
   }
 
   // copy old array to newArray
-  newArray = (FLOAT*) memcpy(newArray, mat->arr, sizeof(FLOAT) * mat->size);
+  newArray = (FLOAT*) memcpy(newArray, vec->arr, sizeof(FLOAT) * vec->size);
 
   // update size of array
-  mat->size *= 2;
+  vec->size *= 2;
 
   // free the old array
-  free(mat->arr);
+  free(vec->arr);
 
   // set pointer to new array in memory
-  mat->arr = newArray;
+  vec->arr = newArray;
 }
 
 /**
@@ -237,23 +108,23 @@ void doubleArraySize(Matrix *mat) {
 * This function reads in the characters and converts them to floats or doubles
 * before storing in the array.
 */
-void storeMatrixToArray(Matrix *mat){
+void storeVectorToArray(Vector *vec){
   int mmapIdx = 0, bfrIdx = 0, arrIdx = 0, numRows = 0, numCols = 0, 
       countCols = 1;
   char buffer[101];    // buffer to hold FLOAT up to 100 digits long
 
-  for(mmapIdx = 0; mmapIdx <= mat->mmapFileSize; mmapIdx++) {
-    if(mat->mmapFileLoc[mmapIdx] == ' '){ // found a number, store into Matrix
+  for(mmapIdx = 0; mmapIdx <= vec->mmapFileSize; mmapIdx++) {
+    if(vec->mmapFileLoc[mmapIdx] == ' '){ // found a number, store into Vector
       // null-terminate the buffer so it can be passed to atof()
       buffer[bfrIdx] = '\0';
 
       // if array is full, double size
-      if (arrIdx == mat->size) {
-        doubleArraySize(mat);
+      if (arrIdx == vec->size) {
+        doubleArraySize(vec);
       }
 
-      // convert char buffer to FLOAT and store in Matrix array
-      mat->arr[arrIdx++] = (FLOAT) atof(buffer);
+      // convert char buffer to FLOAT and store in Vector array
+      vec->arr[arrIdx++] = (FLOAT) atof(buffer);
 
       // only count number of columns until reach first newline
       if (countCols) numCols++;
@@ -261,9 +132,9 @@ void storeMatrixToArray(Matrix *mat){
       // clear buffer, reset buffer index to 0
       memset(buffer, '\0', bfrIdx);
       bfrIdx = 0;
-    } else if(mat->mmapFileLoc[mmapIdx] == '\n') {
+    } else if(vec->mmapFileLoc[mmapIdx] == '\n') {
       if (countCols) {
-        mat->cols = numCols;
+        vec->cols = numCols;
         countCols = 0;    // disable counting columns
       }
       numRows++;
@@ -271,52 +142,13 @@ void storeMatrixToArray(Matrix *mat){
 
     /* grab a character at each loop iteration and store into buffer[] to 
     conv to FLOAT */
-    buffer[bfrIdx++] = mat->mmapFileLoc[mmapIdx];
+    buffer[bfrIdx++] = vec->mmapFileLoc[mmapIdx];
   }
 
-  mat->rows = numRows;
+  vec->rows = numRows;
 }
 
-int errorCheckMatrices(Matrix *mat1, Matrix *mat2){
-  // check that matrix1 is same size as matrix2
-  if(mat1->cols != mat2->rows){
-    fprintf(stderr, "mat1:%dx%d and mat2:%dx%d\n", mat1->rows, mat1->cols,
-	    mat2->rows, mat2->cols);
-    printf("Error: matrices are not compatible size\n");
-    exit(1);
-  }
 
-  return 0;
-}
-
-/**
-* In order for faster matrix processing, this function will take an array
-* of matrix data in row-major order and convert it to column-major order.
-*/
-void convValuesColMajor(Matrix *p) {
-  FLOAT *newArray = (FLOAT *) malloc(sizeof(FLOAT) * p->rows * p->cols);
-
-  if (newArray == NULL) {
-    perror("Error, couldn't allocate space for array");
-    exit(1);
-  }
-
-  int iterator = 0;
-  int i, j, idx;
-  for(i = 0; i < p->rows; i++){
-    idx = i;
-    for(j = 0; j < p->cols; j++){
-      newArray[idx] = p->arr[iterator++];
-      idx += p->rows;
-    }
-  }
-
-  // free the old array
-  free(p->arr);
-
-  // set pointer to new array
-  p->arr = newArray;
-}
 
 /**
  * This function will multiply using the cuBLAS library.
@@ -328,43 +160,9 @@ int main( int argc, char **argv ) {
     printf("Error: incorrect amount of arguments");
     exit(1);
   }
-
-  // Setup matrix1
-  Matrix m1;
-  Matrix *pMat1 = &m1;
-  mapFileToMemory(argv[1], pMat1);
-  initMatrixArray(pMat1, DEF_ROWS, DEF_COLS);
-  storeMatrixToArray(pMat1);
-  unmapFile(pMat1);
-
-  // Setup matrix2
-  Matrix m2;
-  Matrix *pMat2 = &m2;
-  mapFileToMemory(argv[2], pMat2);
-  initMatrixArray(pMat2, DEF_ROWS, DEF_COLS);
-  storeMatrixToArray(pMat2);
-  if (COL_MAJOR)
-    convValuesColMajor(pMat2);
-  unmapFile(pMat2);
-
-  // check for errors
-  errorCheckMatrices(pMat1, pMat2);
-
-  // Matrix3 setup and compute
-  Matrix m3;
-  Matrix *pMat3 = &m3;
-  initMatrixArray(pMat3, pMat1->rows, pMat2->cols);
-  if(!COL_MAJOR) 
-    computeMatrix(pMat1, pMat2, pMat3);
-  else
-    computeMatrixPrime(pMat1, pMat2, pMat3);
   
-  writeOutput(pMat3);
-
+    
   // Free allocated memory
-  free(pMat1->arr);
-  free(pMat2->arr);
-  free(pMat3->arr);
 
   return 0;
 }
